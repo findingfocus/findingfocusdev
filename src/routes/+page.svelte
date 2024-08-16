2691739f0a9525eed652bc8bafa9aace7c2979bd
Paul Thompson <paul@findingfocus.dev>
2024-08-16 13:10:12 -0600

    add favicon and tailwind installation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl hover:text-sky-400">FindingFocus.dev with Svelte</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
