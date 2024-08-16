7f1f24133e7c0ec94514ea3c4ca1bb3396949524
Paul Thompson <paul@findingfocus.dev>
2024-08-16 15:51:17 -0600

    added navbar styling and flowbite svelte navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class', // Enable class-based dark mode
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    'node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1D4ED8', // Custom primary color
+        secondary: '#9333EA', // Custom secondary color
+        navbarBg: '#4D5578', // Custom navbar background color
+        navbarBgDark: '#1F2336', // Custom navbar background color for dark mode
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
